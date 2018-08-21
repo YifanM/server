478a29aff42e7a07b4d0ce90d60e5be692530efc
Yifan Ma <=>
2018-08-21 00:04:45 -0700

    asd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ const app = express();
 
 app.get('/api', (req, res) => res.send('helloooo'));
 
-app.use(express.static('public'));
-
 app.listen(3000, () => console.log('Running on localhost:3000'));
